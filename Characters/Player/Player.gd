72829544966a3a4487ebb719c7b3a3c6315542aa
Roan Morgan <roanlm007@gmail.com>
2024-10-07 16:49:04 -0400

    the dude turns left and right now

--- Characters/Player/Player.gd
+++ Characters/Player/Player.gd
@@ -1,11 +1,1 @@
 extends Character
-
-func get_input() -> void:
-	if Input.is_action_pressed("ui_down"):
-		mov_direction += Vector2.DOWN
-	if Input.is_action_pressed("ui_left"):
-		mov_direction += Vector2.LEFT
-	if Input.is_action_pressed("ui_right"):
-		mov_direction += Vector2.RIGHT
-	if Input.is_action_pressed("ui_up"):
-		mov_direction += Vector2.UP
--- Characters/character.gd
+++ Characters/character.gd
@@ -4,8 +4,13 @@ class_name Character
 # script to animate the character with the given set of images
 @onready var _animated_sprite = $AnimatedSprite2D
 func _process(_delta):
-	if Input.is_action_pressed("left") or Input.is_action_pressed("right") or Input.is_action_pressed("up") or Input.is_action_pressed("down"):
-		_animated_sprite.play("default")
+	if Input.is_action_pressed("right") or Input.is_action_pressed("up") or Input.is_action_pressed("down"):
+		_animated_sprite.play("run")
+		_animated_sprite.flip_h = false
+		
+	elif Input.is_action_pressed("left"):
+		_animated_sprite.flip_h = true
+		
 	else:
 		_animated_sprite.stop()
 
