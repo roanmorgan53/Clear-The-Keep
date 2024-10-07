75e61d86290a8d8dedf999ed8b5ea6ae4109a1ec
Roan Morgan <roanlm007@gmail.com>
2024-10-07 17:55:48 -0400

    removed the character class because it was unused

--- Characters/character.gd
+++ Characters/character.gd
@@ -4,11 +4,11 @@ class_name Character
 # script to animate the character with the given set of images
 @onready var _animated_sprite = $AnimatedSprite2D
 func _process(_delta):
-	if Input.is_action_pressed("right") or Input.is_action_pressed("up") or Input.is_action_pressed("down"):
+	if Input.is_action_pressed("right") or Input.is_action_pressed("up"):
 		_animated_sprite.play("run")
 		_animated_sprite.flip_h = false
 		
-	elif Input.is_action_pressed("left"):
+	elif Input.is_action_pressed("left") or Input.is_action_pressed("down"):
 		_animated_sprite.flip_h = true
 		
 	else:
