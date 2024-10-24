be01402da411d93cc368f42aaec6f240e777c24d
Roan Morgan <roanlm007@gmail.com>
2024-10-24 10:36:29 -0400

    started on the base enemy scene

---
+++ Scenes/enemy.gd
@@ -0,0 +1,7 @@
+@icon("res://Assets/Art/0x72_DungeonTilesetII_v1.7/0x72_DungeonTilesetII_v1.7/frames/goblin_idle_anim_f3.png")
+extends Character
+
+# TODO: Pathfiding for the enemy
+# TODO: Find a way to animate damage
+
+var health: int = 100
