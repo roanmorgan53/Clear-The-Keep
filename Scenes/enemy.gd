739465c62fb95af3a88b43c72832d874cdfbb09f
roanmorgan53 <roanlm007@gmail.com>
2024-11-03 16:26:58 -0500

    Organized Nodes Started work on damage taking

--- Scenes/enemy.gd
+++ Scenes/enemy.gd
@@ -1,7 +1,8 @@
 @icon("res://Assets/Art/0x72_DungeonTilesetII_v1.7/0x72_DungeonTilesetII_v1.7/frames/goblin_idle_anim_f3.png")
 extends Character
 
-# TODO: Pathfiding for the enemy
+# TODO: Begin pathfinding after the player has entered their agression area
+# TODO: Make individual enemies, like zombie, ogre, and such 
 # TODO: Find a way to animate damage
 
 @export var player: Node2D
@@ -11,7 +12,12 @@ extends Character
 var health: int = 100
 
 func _ready() -> void:
-	player = get_parent().find_child("Player")
+	player = get_parent().get_parent().find_child("Player")
+	
+	if player:
+		print("Player node found and assigned")
+	else:
+		print("Player node not found; make sure it's named correctly in the scene")
 
 func _physics_process(delta: float) -> void:
 	var dir = to_local(navigation_agent_2d.get_next_path_position()).normalized()
@@ -22,11 +28,7 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	
 func make_path() -> void:
-	if (player.global_position):
-		navigation_agent_2d.target_position = player.global_position
-	print("Making path...")
-	print("Player Pos = ", navigation_agent_2d.target_position)
+	navigation_agent_2d.target_position = player.global_position
 
 func _on_timer_timeout() -> void:
 	make_path()
-	print("Attempting to make path")
--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -14,4 +14,14 @@ var mov_direction: Vector2 = Vector2.ZERO # vector initialized to zero
 func move() -> void:
 	mov_direction = mov_direction.normalized()
 	velocity += mov_direction * acceleration
-	
+
+# A function to flash red
+func flash_red() -> void:
+	$AnimatedSprite2D.modulate = Color(1, 0, 0)  # Set the sprite color to red
+	await get_tree().create_timer(0.1).timeout  # Wait 0.1 seconds
+	$AnimatedSprite2D.modulate = Color(1, 1, 1)  # Reset the sprite color to normal
+
+# Call this function when your character takes damage
+func take_damage(amount) -> void:
+	# Process damage (e.g., reduce health)
+	flash_red()  # Flash red to indicate damage
